0201a03f772eb1f6b69297a8f4ddf91447a05b8a
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2021-10-18 20:22:08 +0300

    Урок 3 часть 2

--- core/src/gb/ru/screen/MenuScreen.java
+++ core/src/gb/ru/screen/MenuScreen.java
@@ -1,27 +1,41 @@
 package gb.ru.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 import gb.ru.base.BaseScreen;
+import gb.ru.math.Rect;
+import gb.ru.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
+    private Texture bg;
     private Texture img;
     private Vector2 pos;
+
+    private Background background;
 
     @Override
     public void show() {
         super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+
         img = new Texture("badlogic.jpg");
         pos = new Vector2();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
+        background.draw(batch);
         batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
         batch.end();
     }
@@ -29,6 +43,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void dispose() {
         super.dispose();
+        bg.dispose();
         img.dispose();
     }
 
