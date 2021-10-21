b3cdb53a6e433145049416780d1a63bff3adbc8a
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2021-10-21 19:43:07 +0300

    Урок 4 часть 1

--- core/src/gb/ru/screen/MenuScreen.java
+++ core/src/gb/ru/screen/MenuScreen.java
@@ -1,36 +1,43 @@
 package gb.ru.screen;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import gb.ru.base.BaseScreen;
 import gb.ru.math.Rect;
 import gb.ru.sprite.Background;
-import gb.ru.sprite.Logo;
+import gb.ru.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 256;
+
+    private TextureAtlas atlas;
     private Texture bg;
-    private Texture img;
 
     private Background background;
-    private Logo logo;
+    private Star stars[];
 
     @Override
     public void show() {
         super.show();
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
-
-        img = new Texture("badlogic.jpg");
-        logo = new Logo(img);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
-        logo.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
@@ -44,23 +51,26 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
-        img.dispose();
+        atlas.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        logo.touchDown(touch, pointer, button);
         return false;
     }
 
     private void update(float delta) {
-        logo.update(delta);
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
-        logo.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
         batch.end();
     }
 
