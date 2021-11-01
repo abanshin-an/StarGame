0c4e698964ed44e59b1b52ffac837f3995ae372c
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2021-11-01 20:03:58 +0300

    Урок 7

--- core/src/gb/ru/sprite/EnemyShip.java
+++ core/src/gb/ru/sprite/EnemyShip.java
@@ -7,11 +7,13 @@ import com.badlogic.gdx.math.Vector2;
 import gb.ru.base.Ship;
 import gb.ru.math.Rect;
 import gb.ru.pool.BulletPool;
+import gb.ru.pool.ExplosionPool;
 
 public class EnemyShip extends Ship {
 
-    public EnemyShip(BulletPool bulletPool, Rect worldBounds, Sound bulletSound) {
+    public EnemyShip(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds, Sound bulletSound) {
         this.bulletPool = bulletPool;
+        this.explosionPool = explosionPool;
         this.worldBounds = worldBounds;
         this.bulletSound = bulletSound;
         this.bulletV = new Vector2();
@@ -23,6 +25,11 @@ public class EnemyShip extends Ship {
     @Override
     public void update(float delta) {
         super.update(delta);
+        if (getTop() < worldBounds.getTop()) {
+            v.set(v0);
+        } else {
+            reloadTimer = reloadInterval * 0.8f;
+        }
         if (getBottom() < worldBounds.getBottom()) {
             destroy();
         }
@@ -40,7 +47,7 @@ public class EnemyShip extends Ship {
             float height
     ) {
         this.regions = regions;
-        this.v.set(v);
+        this.v0.set(v);
         this.bulletRegion = bulletRegion;
         this.bulletHeight = bulletHeight;
         this.bulletV.set(bulletV);
@@ -48,5 +55,13 @@ public class EnemyShip extends Ship {
         this.hp = hp;
         this.reloadInterval = reloadInterval;
         setHeightProportion(height);
+        this.v.set(0, -0.5f);
+    }
+
+    public boolean isBulletCollision(Bullet bullet) {
+        return !(bullet.getRight() < getLeft()
+                || bullet.getLeft() > getRight()
+                || bullet.getBottom() > getTop()
+                || bullet.getTop() < pos.y);
     }
 }
